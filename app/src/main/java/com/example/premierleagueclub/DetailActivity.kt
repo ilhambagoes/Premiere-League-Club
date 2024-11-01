f734bf4677d083628e74a3fdb8b2013a8c6ec62d
ilhambagoes <ilham.tripoetra@mncbank.co.id>
2024-11-01 10:56:11 +0700

    Add detail in detail page

--- app/src/main/java/com/example/premierleagueclub/DetailActivity.kt
+++ app/src/main/java/com/example/premierleagueclub/DetailActivity.kt
@@ -1,10 +1,13 @@
 package com.example.premierleagueclub
 
+import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class DetailActivity : AppCompatActivity() {
 
@@ -12,12 +15,18 @@ class DetailActivity : AppCompatActivity() {
         const val EXTRA_DATA = "EXTRA_DATA"
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
         val ivClubLogo: ImageView = findViewById(R.id.club_logo)
-        val tvClubName: TextView = findViewById(R.id.club_name)
         val tvClubDescription: TextView = findViewById(R.id.club_description)
         val tvClubStadium: TextView = findViewById(R.id.club_stadium)
         val tvKeyPlayer: TextView = findViewById(R.id.key_player)
@@ -26,22 +35,33 @@ class DetailActivity : AppCompatActivity() {
         val tvTrophies: TextView = findViewById(R.id.trophies)
         val tvWebsite: TextView = findViewById(R.id.website)
 
-        val club  = if (Build.VERSION.SDK_INT >= 33) {
+        val club = if (Build.VERSION.SDK_INT >= 33) {
             intent.getParcelableExtra(EXTRA_DATA, Club::class.java)
         } else {
             intent.getParcelableExtra(EXTRA_DATA)
         }
 
         if (club != null) {
+            supportActionBar?.title = club.name
             ivClubLogo.setImageResource(club.photo)
-            tvClubName.text = club.name
             tvClubDescription.text = club.description
             tvClubStadium.text = club.stadium
-            tvKeyPlayer.text = club.keyPlayer
-            tvManager.text = club.manager
-            tvFoundedYear.text = club.foundedYear
-            tvTrophies.text = club.trophies
+            tvKeyPlayer.text = "Pemain Kunci : ${club.keyPlayer}"
+            tvManager.text = "Manager : ${club.manager}"
+            tvFoundedYear.text = "Tahun Berdiri : ${club.foundedYear}"
+            tvTrophies.text = "Jumlah Trofi : ${club.trophies} Trofi"
             tvWebsite.text = club.website
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
